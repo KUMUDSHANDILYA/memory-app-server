9f1554db8948ebc4a9d24bcc7c566ca20a89fe0a
Kumud Shandilya <kumudshandilyag@gmail.com>
2021-03-21 10:40:34 +0530

    Update post

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
+
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = `mongodb+srv://kumud:kumud@cluster0.wlzyv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
