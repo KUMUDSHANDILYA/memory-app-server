791d8235a52ac171fe87f07cc6a20e2b6bdc0482
Kumud Shandilya <kumudshandilyag@gmail.com>
2021-03-21 12:43:13 +0530

    Environment variable

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
@@ -16,11 +16,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = `mongodb+srv://kumud:kumud@cluster0.wlzyv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
 const PORT = process.env.PORT || 8000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(()=> app.listen(PORT, () => console.log(`Server running on Port ${PORT}`)))
   .catch((error)=> {console.log(error.message)});
 
